f43fb4337bcb2d5642e6f0ab97a20c3d088dfdd2
Sourabh1601 <156401300+Sourabh1601@users.noreply.github.com>
2025-06-14 17:54:21 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'indic-lang-detect-v1';
 const urlsToCache = [
   '/',
   '/static/icon.png',
-  '/templates/index.html',
   '/static/sw.js',
   '/manifest.json'
 ];
@@ -18,5 +17,20 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
   );
 });
